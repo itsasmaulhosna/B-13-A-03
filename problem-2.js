283f0ff42f7c269b216e04df202e9a1a790d0cda
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-02-06 22:06:31 +0600

    Otp check

---
+++ problem-2.js
@@ -0,0 +1,17 @@
+/*function signature/sample */
+function validOtp(otp) {
+  // You have to write your code here
+  if (typeof otp !== 'string') {
+    return 'invalid';
+  }
+  let isValid = false;
+  let length = otp.length;
+  if (length === 8 && otp.startsWith('ph-')) {
+    isValid = true;
+  }
+  return isValid;
+}
+
+const otp = 'ph-10985';
+const result = validOtp(otp);
+console.log(result);
